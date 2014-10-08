65be4c74de76a5280b9571b5ee237f5bab53306a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-10-08 11:08:19 -0700

    [FEATURE] Listening on PORT defined by env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
 	res.send('Hey there, waiting for an SMS');
 });
@@ -9,4 +11,6 @@ app.post('/sms', function(req, res){
   console.log(req);
 });
 
-app.listen(80);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
